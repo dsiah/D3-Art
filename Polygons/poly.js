e41c3c9946a132a74ccfc8f56c3e4db8dc285abe
David Siah <dsiah@villanova.edu>
2015-04-12 19:41:56 -0400

    more shapes

--- Polygons/poly.js
+++ Polygons/poly.js
@@ -24,7 +24,7 @@
     };
   }
 
-  var shapes = [ 3, 4, 5, 6, 7, 8, 9, 10 ];
+  var shapes = [ 3, 4, 5, 6, 7, 8, 9, 10, 12, 25, 50, 100 ];
   
   var svg = d3.select('svg')
       .attr({
@@ -38,25 +38,24 @@
       .append('g')
       .attr({ // change to official transform
         x: function (d, i) {
-          return i % 4 * 50;
+          return i % 4 * 70;
         },
         y: function (d, i) {
-          return Math.floor(i / 4) * 60;
+          return Math.floor(i / 4) * 75;
         },
         transform: function (d, i) {
           var ex = (i % 4) * 50;
-          var why = Math.floor(i / 4) * 60 + 10;
+          var why = Math.floor(i / 4) * 75 + 10;
           
           return 'translate(' + ex + ',' + why + ')';
         }
       });
   
-  var rects = groups.append('rect')
+  var rects = groups.append('circle')
       .attr({
-        width: 40,
-        height: 40,
-        x: 0,
-        y: 10
+        r: 20,
+        cx: 22,
+        cy: 30
       });
   
   var text = groups.append('text')
@@ -71,6 +70,19 @@
       })
       .style('font-family', 'Arial')
       .style('font-size', 12);
+
+  var centers = groups.append('circle')
+      .attr({
+        r: 1,
+        cx: function () {
+          var parentX = Number(this.parentNode.attributes[0].nodeValue);
+          return 25;
+        },
+        cy: function () {
+          return 30;
+        }
+      })
+      .style('fill', "red");
 
   // GOALS:
   // Eventually will create mapping objects to put in the shapes array
